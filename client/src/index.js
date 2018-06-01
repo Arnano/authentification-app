cc5e17517eb2c84c5a844b0c1a80b77410527f0d
Arnaud Bourdillon <arnaud.bourdillon@yahoo.fr>
2018-06-02 00:02:12 +0100

    Wiring up signup form

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import App from "./components/App";
+import Signup from "./components/auth/Signup";
 import { Title } from "./components/Title";
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       <Route path="/" exact component={Title} />
+      <Route path="/signup" component={Signup} />
     </App>
   </BrowserRouter>,
   document.getElementById("root")
